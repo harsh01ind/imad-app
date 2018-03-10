d56745afe7e55cf30e9012f8676f3a39d59b3d67
Harsh Nayak <33658746+harsh01ind@users.noreply.github.com>
2018-03-10 15:16:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ var articles=
     }
 };
 
-app.get('/articlename', function (req, res) {
-  res.send(articles[articlename])
+app.get(':/articlename', function (req, res) {
+  res.send(createTemplate(articles[articlename]))
 });
 app.get('/article1', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
