1cfa028accf9ebba6cae924acb83bd6d32fc17c5
Harsh Nayak <33658746+harsh01ind@users.noreply.github.com>
2018-03-11 21:36:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,24 @@
-console.log('Loaded!');
-alert('this is my first webpage');
-var element= document.createElementById('text');
-element.innerHTML='harsh is my name';
+alert('this is my first web page');
+
+
+var submit=document.getElementById('submit');
+submit.onclick= function(){
+    var nameInput=document.getElementById('input');
+    var name=nameInput.value;
+    var request=new XMLHttpRequest();
+   
+    request.onload = function(){
+        var names=request.responseText;
+        names=JSON.parse(names);
+        var list='';
+        for(var i=0; i<names.length;i++)
+        {
+        list= '<li>' +names[i]+ '<li>';
+        }
+        var ul= document.getElemnetById('list');
+        ul.innerHTML=list;
+    };
+     request.open('GET', 'http://harshnyk190398.imad.hasura-app.io/hello?hello'+name,true);
+    request.send(null);
+};
+    
